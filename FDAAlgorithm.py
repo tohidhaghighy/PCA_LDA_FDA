885f120ca65ddc4baf4a1b25ac4172ab234facc8
tohid1 <tohidhaghighi@gmail.com>
2020-01-06 22:49:09 +0330

    Description Program

--- FDAAlgorithm.py
+++ FDAAlgorithm.py
@@ -17,20 +17,25 @@ class FDA:
     def get_lda_score(self,X,MU_k,SIGMA,pi_k): 
         #Returns the value of the linear discriminant score function for a given class "k" and 
         # a given x value X
+        print("-----------------------------")
+        print(X)
+        print(MU_k)
+        print(SIGMA)
+        print(pi_k)
         return (np.log(pi_k) - 1/2 * (MU_k).T @ np.linalg.inv(SIGMA)@(MU_k) + X.T @ np.linalg.inv(SIGMA)@ (MU_k)).flatten()[0]
 
     def predict_lda_class(self,X,MU_list,SIGMA,pi_list, y):
         scores_list = []
         classes = np.unique(y)
         for p in range(len(classes)):
-            score = get_lda_score(X.reshape(-1,1),MU_list[p].reshape(-1,1),SIGMA,pi_list[0]) 
+            score = self.get_lda_score(X.reshape(-1,1),MU_list[p].reshape(-1,1),SIGMA,pi_list[0]) 
             scores_list.append(score)
         self.FDA_List=scores_list
         return classes[np.argmax(scores_list)]
 
     def PlayFDA(self,x,y):
-        X1 = x.iloc[120, :].values.reshape(4, -1)
-        pi_k = [np.count_nonzero(y==1)/len(y) for x in range(3)]
+        X1 = x.iloc[120, :].values.reshape(2, -1)
+        pi_k = [50/len(y) for x in range(3)]
         sigma = x.cov().values
         mean_vectors = []
         for c in np.unique(y):
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from PCAAlgorithm import PCA
 from LDAAlgorithm import LDA
 from FDAAlgorithm import FDA
+import pandas as pd
+import numpy as np
 
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
@@ -13,6 +15,7 @@ pca.PlayPCA(df)
 
 # pca.pca_matrix
 
+pcalist=pca.pca_matrix
 
 #----------------------run lda ------------------------
 lda=LDA()
@@ -21,4 +24,15 @@ x = df.loc[:, 0:3]
 y = df.loc[:, [4]]
 lda.Play_LDA(x,y)
 
+ldalist=lda.LDA_Matrix_list
+
 #lda.LDA_Matrix_list
+
+# xpca=pca.loc[:,0:1]
+# xlda=lda.loc[:,0:1]
+xpca=pd.DataFrame(pcalist)
+xlda=pd.DataFrame(ldalist)
+print(xpca)
+print(xlda)
+fda=FDA()
+fda.PlayFDA(xpca,y)
